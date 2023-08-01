0bad7709721af5bbfb9a16b219998576e2339624
hshehabu <hamzashehabu457@gmail.com>
2023-08-01 08:50:27 +0300

    designing the todo screen

---
+++ lib/screens/taskScreen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: const Icon(
+          Icons.add,
+          size: 30.0,
+        ),
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  topLeft: Radius.circular(20.0),
+                ),
+              ),
+              child: ListView(
+                children: const [
+                  ListTile(
+                    title: Text('data'),
+                    trailing: Checkbox(value: false, onChanged: null),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
