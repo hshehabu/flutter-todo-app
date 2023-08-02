5e4d8807827f83026a3e06240a3f91c31b9d1bca
hshehabu <hamzashehabu457@gmail.com>
2023-08-02 19:52:03 +0300

    callback when checkbox checked

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -10,24 +10,38 @@ class TaskTile extends StatefulWidget {
 
 class _TaskTileState extends State<TaskTile> {
   bool isDone = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(widget.taskName),
-      trailing:  TaskCheckbox(isDone:isDone,),
+      title: Text(
+        widget.taskName,
+        style:
+            TextStyle(decoration: isDone ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+        isDone: isDone,
+        toggleCheckboxState: (bool? checkboxState) {
+          setState(() {
+            isDone = checkboxState ?? true;
+          });
+        },
+      ),
     );
   }
 }
 
 class TaskCheckbox extends StatelessWidget {
-  const TaskCheckbox({ required this.isDone});
-final bool isDone;
+  const TaskCheckbox({super.key, required this.isDone, required this.toggleCheckboxState});
+
+  final bool isDone;
+  final Function(bool?) toggleCheckboxState;
+
   @override
   Widget build(BuildContext context) {
     return Checkbox(
-        value: isDone,
-        onChanged: (value) {
-        });
+      value: isDone,
+      onChanged: toggleCheckboxState,
+    );
   }
 }
-
