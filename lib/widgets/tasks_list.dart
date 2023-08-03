1db75f93694801121bc60a9ced4896f44adfdf59
hshehabu <hamzashehabu457@gmail.com>
2023-08-03 07:54:23 +0300

    Tasks list to a stateful widget

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+import '../models/task.dart';
+
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [];
   @override
   Widget build(BuildContext context) {
     return ListView(
