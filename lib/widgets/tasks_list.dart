280fc535a810cad6213a5c9879917513c9f8cb69
hshehabu <hamzashehabu457@gmail.com>
2023-08-08 10:01:29 +0300

    changed to stateless and used the data from the provider

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/widgets/task_tile.dart';
-
+import 'package:provider/provider.dart';
+import 'package:flutter_todo_app/models/text_data.dart';
 import '../models/task.dart';
 
-class TasksList extends StatefulWidget {
-  const TasksList(this.tasks);
-  final List<Task> tasks;
-  @override
-  State<TasksList> createState() => _TasksListState();
-}
-
-class _TasksListState extends State<TasksList> {
+class TasksList extends StatelessWidget {
+  TasksList(List<Task> tasks);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          isDone: widget.tasks[index].isDone,
-          taskTitle: widget.tasks[index].name,
-          checkboxCallback: (checkboxState) {
-            setState(() {
-              widget.tasks[index].toggleDone();
-            });
+    return Consumer<TaskData>(
+      builder: ( context, taskData, child) {
+        return  ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isDone: taskData.tasks[index].isDone,
+              taskTitle: taskData.tasks[index].name,
+              checkboxCallback: (checkboxState) {
+                // setState(() {
+                //   taskData.tasks[index].toggleDone();
+                // });
+              },
+            );
           },
+          itemCount: taskData.getTaskLength(),
         );
       },
-      itemCount: widget.tasks.length,
     );
   }
 }
