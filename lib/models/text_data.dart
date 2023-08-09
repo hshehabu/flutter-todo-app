385d45c468600eb97be399561d8f21d9ad32bf1e
hshehabu <hamzashehabu457@gmail.com>
2023-08-09 09:34:02 +0300

    change tasks to private and make it still accessible with unmodifiable getter

--- lib/models/text_data.dart
+++ lib/models/text_data.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_todo_app/models/task.dart';
+import 'package:collection/collection.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  final List<Task> _tasks = [
     Task(name: 'buy bread', isDone: false),
     Task(name: 'buy egg', isDone: false),
     Task(name: 'buy tuna', isDone: false),
   ];
 
-  int getTaskLength() => tasks.length;
+  UnmodifiableListView<Task> get tasks => UnmodifiableListView(_tasks);
+
+  int getTaskLength() => _tasks.length;
+
+  void addTask(String newTaskTitle){
+
+    final newTask = Task(name: newTaskTitle, isDone: false);
+    _tasks.add(newTask);
+    notifyListeners();
+  }
+
+  void updateTask(Task task){
+   task.toggleDone();
+   notifyListeners();
+  }
 }
